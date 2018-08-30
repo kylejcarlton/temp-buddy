a3682ca65da552e24202713b9ebbf2ba8fb13316
Kyle J Carlton <kylejcarlton@gmail.com>
2018-08-29 20:00:52 -0500

    remove battery state indicator

--- script.js
+++ script.js
@@ -5,10 +5,12 @@ $(document).ready(function(){
         $("#temp").html("&nbsp;" + result.values[2][1] + "&nbsp;&#8457;");
         $("#humid").html("&nbsp;" + result.values[3][1] + " % RH");
         if(result.values[5][1] == "unknown"){
-            $("#bat").html("&nbsp;" + result.values[4][1] + "%" + " - Discharging");
+            /*$("#bat").html("&nbsp;" + result.values[4][1] + "%" + " - Discharging");*/
+            $("#bat").html("&nbsp;" + result.values[4][1] + "%");
         }
         else if(result.values[5][1] == "VIN" || result.values[5][1] == "USB host"){
-            $("#bat").html("&nbsp;" + result.values[4][1] + "%" + " - Charging");
+            /*$("#bat").html("&nbsp;" + result.values[4][1] + "%" + " - Charging");*/
+            $("#bat").html("&nbsp;" + result.values[4][1] + "%");
         }
         $("#otemp").html("&nbsp;" + result.values[7][1] + "&nbsp;&#8457;");
         $("#ohumid").html("&nbsp;" + result.values[8][1] + " % RH");
