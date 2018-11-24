98c40d41f57c7ad532c1d5135ca2bea4c838540d
kylejcarlton <kylejcarlton@gmail.com>
2018-11-23 23:35:48 -0600

    display power source

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ $(document).ready(function(){
         $("#temp").html("&nbsp;" + result.values[2][1] + "&nbsp;&#8457;");
         $("#humid").html("&nbsp;" + result.values[3][1] + " % RH");
         if(result.values[5][1] == "unknown"){
-            /*$("#bat").html("&nbsp;" + result.values[4][1] + "%" + " - Discharging");*/
-            $("#bat").html("&nbsp;" + result.values[4][1] + "%");
+            $("#bat").html("&nbsp;" + result.values[4][1] + "%" + "<br><br><i class='fas fa-plug'></i>&nbsp;<span style='color:red;'><i class='fas fa-times'></i></span>");
+            //$("#bat").html("&nbsp;" + result.values[4][1] + "%");
         }
         else if(result.values[5][1] == "VIN" || result.values[5][1] == "USB host"){
-            /*$("#bat").html("&nbsp;" + result.values[4][1] + "%" + " - Charging");*/
-            $("#bat").html("&nbsp;" + result.values[4][1] + "%");
+            $("#bat").html("&nbsp;" + result.values[4][1] + "%" + "<br><br><i class='fas fa-plug'></i>&nbsp;<span style='color:green;'><i class='fas fa-check'></i></span>");
+            //$("#bat").html("&nbsp;" + result.values[4][1] + "%");
         }
         $("#otemp").html("&nbsp;" + result.values[7][1] + "&nbsp;&#8457;");
         $("#ohumid").html("&nbsp;" + result.values[8][1] + " % RH");
